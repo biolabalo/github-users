e45fa627764a19a4e205483016c7a08910fb5dff
biola balogun <balogunbiola101@gmail.com>
2021-10-26 12:06:13 +0100

    implement tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import userEvent from "@testing-library/user-event";
+import UserSearch from "./Components/userSearch";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('On page load the button is disabled', async () => {
+    render(<UserSearch  />);  
+    expect(screen.getByRole('button', { name: /Search/ })).toBeDisabled();
+
+});
+
+test('When a word is inputed  the button is enabled', async () => {
+  render(<UserSearch  />);  
+  userEvent.type(screen.getByPlaceholderText(/Enter a user name/i), "biolabalo");
+  expect(await screen.findByRole('button', { name: /Search/i })).toBeEnabled();
+
+});
+
+
+test('Input should accept text',  () => {
+  render(<UserSearch  />);  
+  const inputNode  = screen.getByPlaceholderText(/Enter a user name/i);
+  expect(inputNode.value).toMatch("")
+  userEvent.type(screen.getByPlaceholderText(/Enter a user name/i), "biolabalo");
+  expect(inputNode.value).toMatch("biolabalo")
 });
